64e63aa47a60af900df74b2d00be6ecad6ada0b3
ArtemShikhov <Artem.s.vvvv@mail.ru>
2026-07-07 22:45:40 +0500

    Revert "Revert "Complete blog customizer implementation with all features""

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,26 +1,51 @@
-import { CSSProperties } from 'react';
+import { CSSProperties, useState, useEffect } from 'react';
 import clsx from 'clsx';
 
 import { Article } from '../article/Article';
 import { ArticleParamsForm } from '../article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './../../constants/articleProps';
+import { defaultArticleState, ArticleStateType } from './../../constants/articleProps';
 
 import styles from './app.module.scss';
 
 export const App = () => {
+	const [articleState, setArticleState] = useState<ArticleStateType>(defaultArticleState);
+
+	// Initialize CSS variables on first render
+	useEffect(() => {
+		const root = document.documentElement;
+		root.style.setProperty('--font-family', defaultArticleState.fontFamilyOption.value);
+		root.style.setProperty('--font-size', defaultArticleState.fontSizeOption.value);
+		root.style.setProperty('--font-color', defaultArticleState.fontColor.value);
+		root.style.setProperty('--container-width', defaultArticleState.contentWidth.value);
+		root.style.setProperty('--bg-color', defaultArticleState.backgroundColor.value);
+	}, []);
+
+	// Apply CSS variables when articleState changes
+	useEffect(() => {
+		const root = document.documentElement;
+		root.style.setProperty('--font-family', articleState.fontFamilyOption.value);
+		root.style.setProperty('--font-size', articleState.fontSizeOption.value);
+		root.style.setProperty('--font-color', articleState.fontColor.value);
+		root.style.setProperty('--container-width', articleState.contentWidth.value);
+		root.style.setProperty('--bg-color', articleState.backgroundColor.value);
+	}, [articleState]);
+
+	const handleApply = (newState: ArticleStateType) => {
+		setArticleState(newState);
+	};
+
+	const handleReset = () => {
+		setArticleState(defaultArticleState);
+	};
+
 	return (
 		<main
-			className={clsx(styles.main)}
-			style={
-				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
-				} as CSSProperties
-			}>
-			<ArticleParamsForm />
+			className={clsx(styles.main)}>
+			<ArticleParamsForm 
+				currentState={articleState} 
+				onApply={handleApply} 
+				onReset={handleReset} 
+			/>
 			<Article />
 		</main>
 	);
